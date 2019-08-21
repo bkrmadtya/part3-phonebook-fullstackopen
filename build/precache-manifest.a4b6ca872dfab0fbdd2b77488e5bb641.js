5791d0bdd9fe45ebb99b84e6ba1967b60d549751
Bikram Karki <himalayanepalinsis@gmail.com>
2019-08-21 14:08:31 +0300

    uibuild

---
+++ build/precache-manifest.a4b6ca872dfab0fbdd2b77488e5bb641.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16748b6bba4aa101dc3dd8e267aad04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "746220bbbe1a8c66379c",
+    "url": "/static/css/main.bd5e5de0.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "746220bbbe1a8c66379c",
+    "url": "/static/js/main.3a28483f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
