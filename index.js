b2eedf11da1e062b85ac167231ce5f1e1f0132f6
Bikram Karki <himalayanepalinsis@gmail.com>
2019-08-21 13:29:30 +0300

    updated port with env.port

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
