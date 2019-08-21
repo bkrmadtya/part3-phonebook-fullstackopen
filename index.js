f0f99d9e7565b7dcb4d76a02ad93e1c065e217e0
Bikram Karki <himalayanepalinsis@gmail.com>
2019-08-21 14:12:25 +0300

    uibuild

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
-app.use(bodyParser.json());
+app.use(express.static('build'));
 app.use(cors());
-app.use(experss.static('build'));
+app.use(bodyParser.json());
 
 morgan.token('body', function(req, res) {
   return JSON.stringify(req.body);
